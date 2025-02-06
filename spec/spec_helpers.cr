37a8c2972b7ee4470baea93e95f522a9ffb6d17f
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-06 20:07:57 +0530

    spec: spec_helpers

---
+++ spec/spec_helpers.cr
@@ -0,0 +1,47 @@
+# -----------------------------------------------------------------------
+# This file is part of MoonScript
+#
+# MoonSript is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# MoonSript is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MoonSript.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2025 Krisna Pranav, MoonScript Developers
+# -----------------------------------------------------------------------
+
+def expect_diff(a, b)
+    a.should eq(b)
+rescue error
+    fail diff(a, b)
+end
+
+def diff(a, b)
+    file1 = File.tempfile do |file|
+        file.puts a.strip
+        file.flush
+    end
+    
+    io = IO::Memory.new
+
+    Process.run("git", [
+        "--no-pages", "diff", "--no-index",
+        file1.path,
+    ], output: io)
+
+    io.to_s
+end
+
+def matches_template(template, expected)
+    expected = expected.uncolarize
+
+    parts = template.split(/x*/)
+
+end
