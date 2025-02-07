512538d103089150f63b7a2a81b7d90e0e4039dd
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-07 18:35:26 +0530

    ext: file

--- src/ext/file.cr
+++ src/ext/file.cr
@@ -17,22 +17,28 @@
 # Copyright (C) 2025 Krisna Pranav, MoonScript Developers
 # -----------------------------------------------------------------------
 
-
 class File
-    def self.write_p(path, contents)
-        FileUtils.make_p File.dirname(path)
-        File.write path, contents
-    end
+  def self.write_p(path, contents)
+    FileUtils.mkdir_p File.dirname(path)
+    File.write path, contents
+  end
 
-    def self.relative_path_from_ancestor(path: String, name: String) : String
-        return path unless directory = File.find_in_ancestors(path, name)
-    end
+  def self.relative_path_from_ancestor(path : String, name : String) : String
+    return path unless directory = File.find_in_ancestors(path, name)
+    Path[path].relative_to(File.dirname(directory)).to_s
+  end
 
-    def self.find_in_ancestors(base: String, name: String) : String?
-        root = File.dirname(base)
+  def self.find_in_ancestors(base : String, name : String) : String?
+    root = File.dirname(base)
 
-        loop do
-          return nil if root == "." || root == "/"
-        end
+    loop do
+      return nil if root == "." || root == "/"
+
+      if File.exists?(path = Path[root, name])
+        return path.to_s
+      else
+        root = File.dirname(root)
+      end
     end
+  end
 end
