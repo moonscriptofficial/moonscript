121cd9a4849b1b05346b08305e803911131fdb45
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-07 20:14:58 +0530

    utils: watcher

--- src/utils/watcher.cr
+++ src/utils/watcher.cr
@@ -18,20 +18,46 @@
 # -----------------------------------------------------------------------
 
 module MoonScript
-    class Watcher
-       @patterns = Array(String) = [] of String
-       @state = Set(Tuple(String, Time)).new
+  class Watcher
+    @patterns : Array(String) = [] of String
+    @state = Set(Tuple(String, Time)).new
 
-       def initialize(&@listener: Proc(Array(String), Symbol, Nil))
-       end
-
-       def patterns=(@patterns: Array(String))
-            @state.clear
-            scan: reset
-       end
-
-       def scan(reason: Symbol)
-        current = Dir.glob(@patterns).map(&->info(String)).sort_by!(&.last).to_set
-       end
+    def initialize(&@listener : Proc(Array(String), Symbol, Nil))
     end
+
+    def patterns=(@patterns : Array(String))
+      @state.clear
+      scan :reset
+    end
+
+    def scan(reason : Symbol)
+      current =
+        Dir.glob(@patterns)
+          .map(&->info(String))
+          .sort_by!(&.last)
+          .to_set
+
+      diff = @state ^ current
+
+      @listener.call(diff.map(&.first).uniq!, reason) unless diff.empty?
+
+      @state = current
+    end
+
+    def info(file : String)
+      path =
+        Path[file].normalize.expand.to_s
+
+      {path, File.info(path).modification_time}
+    end
+
+    def watch
+      spawn do
+        loop do
+          sleep 0.1.seconds
+          scan :modified
+        end
+      end
+    end
+  end
 end
