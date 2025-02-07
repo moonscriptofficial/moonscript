fb034419df7bc3f8fb7e01ddc4a774df7699d182
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-07 20:19:26 +0530

    assets: live-reload

---
+++ src/assets/live-realod.js
@@ -0,0 +1,26 @@
+/**
+ * @file live-reload.js
+ * @author Krisna Pranav
+ * @brief Live Reload Functionality
+ * @version 1.0
+ * @date 2024-02-07
+ * 
+ * @copyright Copyright (c) 2025 Krisna Pranav, Moon Developers
+ * 
+ */
+
+(function connect(reload = false) {
+    let closing = false;
+
+    window.addEventListener("beforeunload", () => {
+        closing = true;
+    });
+
+    var ws = new WebSocket("ws://" + location.host);
+
+    ws.onopen = () => {
+        if (reload) {
+            window.location.reload();
+        }
+    };
+})
