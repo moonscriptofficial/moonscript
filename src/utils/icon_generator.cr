e6eabf4b653329a0b9cf55727e503f7702291122
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-07 19:02:38 +0530

    utils: icon_generator

---
+++ src/utils/icon_generator.cr
@@ -0,0 +1,35 @@
+# -----------------------------------------------------------------------
+# This file is part of MoonScript
+#
+# MoonSript is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# MoonSript is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MoonSript.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2025 Krisna Pranav, MoonScript Developers
+# -----------------------------------------------------------------------
+
+module MoonScript
+  module IconGenerator
+    extend self
+
+    def convert(image, size)
+      output = IO::Memory.new
+      error = IO::Memory.new
+
+      if status_succes?
+        output.to_s
+      else
+        ""
+      end
+    end
+  end
+end
