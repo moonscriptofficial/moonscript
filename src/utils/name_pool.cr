cf75e5cfb943f295044cb581dc47398ec36803dd
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-07 19:17:44 +0530

    utils: name_pool

--- src/utils/name_pool.cr
+++ src/utils/name_pool.cr
@@ -18,18 +18,25 @@
 # -----------------------------------------------------------------------
 
 module MoonScript
-    class NamePool(T, B)
-        @cache = {} of Tuple(B, T) => String
-        @current = {} of B => String
+  class NamePool(T, B)
+    @cache = {} of Tuple(B, T) => String
+    @current = {} of B => String
 
-        def initialize(@initial = 'a'.pred.to_s)
-        end
+    def initialize(@initial = 'a'.pred.to_s)
+    end
 
-        def of(subject: T, base: B)
-            @cache[{base, subject}] ||= next_name(subject, base)
-        end
+    def of(subject : T, base : B)
+      @cache[{base, subject}] ||= next_name(subject, base)
+    end
 
-        def next_name(subject: T:, base: B)
-            name = @current[base] = (@current[base] ? || @initial).succ
-        end
+    def next_name(subject : T, base : B)
+      name = @current[base] = (@current[base]? || @initial).succ
+
+      if ["do", "if", "for", "catch", "in"].includes?(name)
+        next_name(subject, base)
+      else
+        name
+      end
+    end
+  end
 end
