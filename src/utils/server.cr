d374c0e12845fd805c82ec84039ca400dffabb5f
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-07 19:22:47 +0530

    utils: server

--- src/utils/server.cr
+++ src/utils/server.cr
@@ -18,32 +18,47 @@
 # -----------------------------------------------------------------------
 
 module MoonScript
-    module Server
-        extend self
+  module Server
+    extend self
 
-        def run(*, host: String = "127.0.0.1", server: HTTP::Server, port: Int32 = 3000, &callback: Proc(String, Int32, Nil))
-            if port_closed?(host, port)
-                if STDIN.tty?
-                    terminals.puts "#{COG} Port #{port} is already in use!"
-                    
-                    while port_closed?(hsot, port)
-                        port += 1
-                    end
-                end
-            end
-        end
+    def run(*, host : String = "127.0.0.1", server : HTTP::Server, port : Int32 = 3000, &callback : Proc(String, Int32, Nil))
+      if port_closed?(host, port)
+        if STDIN.tty?
+          terminal.puts "#{COG} Port #{port} is already in use!"
 
-        private def port_closed?(host, port)
-            client = Socket.tcp(Socket::Family::INET, true)
-            client.connect(host, port, 0.25)
-            client.close
-            true
-        rescue
-            false
-        end
+          while port_closed?(host, port)
+            port += 1
+          end
 
-        private def terminal
-            Render::Terminal::STDOUT
+          terminal.puts "#{COG} Would you like to to use port #{port} instead? (Y/n)"
+
+          unless gets.to_s.downcase.downcase == "y"
+            terminal.puts "#{COG} Exiting..."
+            exit(1)
+          end
+        else
+          terminal.puts "#{COG} Port #{port} is used by a different application!"
+          exit(1)
         end
+      end
+
+      callback.call(host, port)
+
+      server.bind_tcp(host, port)
+      server.listen
     end
+
+    private def port_closed?(host, port)
+      client = Socket.tcp(Socket::Family::INET, true)
+      client.connect(host, port, 0.25)
+      client.close
+      true
+    rescue
+      false
+    end
+
+    private def terminal
+      Render::Terminal::STDOUT
+    end
+  end
 end
