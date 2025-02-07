95b240460c313167c983a49974be2a75f0ead70f
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-07 21:59:56 +0530

    moon_json: applications[display]

--- src/moon_json/applications/display.cr
+++ src/moon_json/applications/display.cr
@@ -18,26 +18,41 @@
 # -----------------------------------------------------------------------
 
 module MoonScript
-    class MoonJson
-        class Parser
-            DISPLAY_VALUES = %w[fullscreen standalone]
+  class MoonJson
+    class Parser
+      DISPLAY_VALUES =
+        %w[fullscreen standalone minimal-ui browser]
 
-            def parse_application_display: String
-                @parser.location.try do |location|
-                    @parser.read_string.tap do |value|
-                        error! :application_display_mismatch do
-                            block do
-                              
-                            end
-                        end
-                    end
-                rescue JSON::ParseException
-                    error! :application_display_invalid do
-                        snippet snippet_data
-                    end
-                end
-            end
+      def parse_application_display : String
+        @parser.location.try do |location|
+          @parser.read_string.tap do |value|
+            error! :application_display_mismatch do
+              block do
+                text "The"
+                bold "value"
+                text "of the"
+                bold "display field"
+                text "should be one of > "
+              end
 
+              snippet DISPLAY_VALUES.join("\n")
+              snippet "It is here:", snippet_data(location)
+            end unless DISPLAY_VALUES.includes?(value)
+          end
         end
+      rescue JSON::ParseException
+        error! :application_display_invalid do
+          block do
+            text "The"
+            bold "display field"
+            text "of the"
+            bold "application object"
+            text "should be a string"
+          end
+
+          snippet snippet_data
+        end
+      end
     end
+  end
 end
