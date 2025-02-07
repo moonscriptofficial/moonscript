fc72a8b18875fce7c07e4b18745e97217a2c8072
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-07 20:05:55 +0530

    utils: vlq

--- src/utils/vlq.cr
+++ src/utils/vlq.cr
@@ -19,33 +19,43 @@
 
 module MoonScript
   module VLQ
-    VLQ_BASE_SHIFT       = 5
-    VLQ_BASE             = 1 << VLQ_BASE_SHIFT
-    VLQ_BASE_MASK        = VLQ_BASE - 1
+    VLQ_BASE_SHIFT = 5
+
+    VLQ_BASE = 1 << VLQ_BASE_SHIFT
+
+    VLQ_BASE_MASK = VLQ_BASE - 1
+
     VLQ_CONTINUATION_BIT = VLQ_BASE
+
+    BASE64_DIGITS =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".chars
 
     def self.encode(int)
       vlq = to_vlq_signed(int)
       encoded = ""
-      cond = True
+      cond = true
 
       while cond
         digit = vlq & VLQ_BASE_MASK
         vlq >>= VLQ_BASE_SHIFT
         digit |= VLQ_CONTINUATION_BIT if vlq > 0
+        encoded += base64_encode(digit)
         cond = vlq > 0
       end
+
+      encoded
     end
 
     private def self.base64_encode(int)
-      BASE64_DIGITS[int]? || raise ArgumentError.new "#{int} is not valid base64 digit"
+      BASE64_DIGITS[int]? || raise ArgumentError.new "#{int} is not a valid base64 digit"
     end
 
     private def self.to_vlq_signed(int)
-        if int < 0 
-            ((-int) << 1) + 1
-        else
-            int << 1
+      if int < 0
+        ((-int) << 1) + 1
+      else
+        int << 1
+      end
     end
   end
 end
