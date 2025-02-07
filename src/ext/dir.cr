6358487d75542fff2d55993e56a755e54a863639
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-07 18:33:37 +0530

    ext: dir

--- src/ext/dir.cr
+++ src/ext/dir.cr
@@ -18,13 +18,21 @@
 # -----------------------------------------------------------------------
 
 class Dir
-    def self.safe_deleted(directory, &)
-        return unless Dir.exists?(directory)
-        yield
-        FileUtils.rm_rf(directory)
-    end
+  def self.safe_delete(directory, &)
+    return unless Dir.exists?(directory)
+    yield
+    FileUtils.rm_rf(directory)
+  end
 
-    def self.tempdir(&)
-        path = Path[tempdir, Random::Secure.hex]
+  def self.tempdir(&)
+    path =
+      Path[tempdir, Random::Secure.hex]
+
+    begin
+      FileUtils.mkdir_p(path)
+      FileUtils.cd(path) { yield }
+    ensure
+      FileUtils.rm_rf(path)
     end
+  end
 end
