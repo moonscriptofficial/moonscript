493b9a8ce31116f6b9ed760ceaae0ef4f3f00a74
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-07 22:05:00 +0530

    moon_json: applications[head]

--- src/moon_json/applications/head.cr
+++ src/moon_json/applications/head.cr
@@ -18,21 +18,50 @@
 # -----------------------------------------------------------------------
 
 module MoonScript
-    class MoonJson
-        class Parser
-            def parse_application_head: String
-                location = @parser.location
-                head = @parser.read_string
-                path = Path[root, head].to_s
+  class MoonJson
+    class Parser
+      def parse_application_head : String
+        location =
+          @parser.location
 
-                error! :application_head_not_exists do
-                end
+        head =
+          @parser.read_string
 
-                File.read(path)
-            rescue JSON::ParseException
-                error! application_head_invalid do
-                end
-            end
+        path =
+          Path[root, head].to_s
+
+        error! :application_head_not_exists do
+          block do
+            text "The"
+            bold "head"
+            text "field of"
+            bold "the application object"
+            text "points to a file that does not exists."
+          end
+
+          block do
+            text "It should point to an HTML file, which would be injected to the"
+            text "<head> tag of the generated HTML. It is used to include"
+            text "external dependencies like (CSS, JS, analytics, etc...)"
+          end
+
+          snippet snippet_data(location)
+        end unless File.exists?(path)
+
+        File.read(path)
+      rescue JSON::ParseException
+        error! :application_head_invalid do
+          block do
+            text "The"
+            bold "head"
+            text "field of"
+            bold "the application object"
+            text "should be a string, but it's not:"
+          end
+
+          snippet snippet_data
         end
+      end
     end
+  end
 end
