31f17c1c33cb72452a38101c1b0c9208089d7e3c
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-07 22:38:55 +0530

    moon_json: applications[title]

---
+++ src/moon_json/applications/title.cr
@@ -0,0 +1,56 @@
+# -----------------------------------------------------------------------
+# This file is part of MoonScript
+#
+# MoonSript is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# MoonSript is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MoonSript.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2025 Krisna Pranav, MoonScript Developers
+# -----------------------------------------------------------------------
+
+module MoonScript
+  class MoonJson
+    class Parser
+      def parse_application_title : String
+        location =
+          @parser.location
+
+        title =
+          @parser.read_string
+
+        error! :application_title_empty do
+          block do
+            text "The"
+            bold "title"
+            text "field of the"
+            bold "application object should not be empty"
+          end
+
+          snippet snippet_data(location)
+        end if title.empty?
+
+        title
+      rescue JSON::ParseException
+        error! :application_title_invalid do
+          block do
+            text "The"
+            bold "title field"
+            text "of the"
+            bold "application object should be string"
+          end
+
+          snippet snippet_data
+        end
+      end
+    end
+  end
+end
