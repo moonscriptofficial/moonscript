71c368315c0a4b361cd87923a7c828d1651cbbfe
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-07 19:36:18 +0530

    utils: source_files

--- src/utils/source_files.cr
+++ src/utils/source_files.cr
@@ -18,19 +18,49 @@
 # -----------------------------------------------------------------------
 
 module MoonScript
-    module SourceFile
-        extend self
+  module SourceFiles
+    extend self
 
-        def globs(jsons: Array(MoonJson), *, include_tests = false) : Array(String)
-            jsons.flat_map { |json| globs(json, include_tests: include_tests) }
-        end
-
-        def globs(json: MoonJson, *, include_tests = false) : Array(String)
-            if include_tests
-                json.source_directories | json.test_directories
-            else
-                json.source_directories
-            end.map { |dir| glob_pattern(File.dirname(json.path), dir )}
-        end
+    def globs(jsons : Array(MoonJson), *, include_tests = false) : Array(String)
+      jsons.flat_map { |json| globs(json, include_tests: include_tests) }
     end
+
+    def globs(json : MoonJson, *, include_tests = false) : Array(String)
+      if include_tests
+        json.source_directories | json.test_directories
+      else
+        json.source_directories
+      end.map { |dir| glob_pattern(File.dirname(json.path), dir) }
+    end
+
+    def everything(json : MoonJson, *, include_tests = false, dot_env = ".env") : Array(String)
+      packages(json, include_self: true)
+        .flat_map { |item| globs(item, include_tests: include_tests) + [item.path] }
+        .push(Path[File.dirname(json.path), dot_env].to_s)
+    end
+
+    def packages(json : MoonJson, *, include_self = false) : Array(MoonJson)
+      (include_self ? [json] : [] of MoonJson).tap do |jsons|
+        each_package(json) do |package_json|
+          jsons << package_json
+        end
+      end
+    end
+
+    private def each_package(json : MoonJson, &)
+      pattern =
+        Path[
+          File.dirname(json.path),
+          ".", ".moon", "packages", "**", "moon.json",
+        ]
+
+      Dir.glob(pattern).each do |file|
+        yield MoonJson.parse(file)
+      end
+    end
+
+    private def glob_pattern(*dirs : Path | String)
+      Path[*dirs, "**", "*.moon"].to_posix.to_s
+    end
+  end
 end
