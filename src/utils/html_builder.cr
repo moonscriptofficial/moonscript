b9cf48ff2b7529d1c5b97512c964bd9b74fcd720
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-07 18:57:36 +0530

    utils: html_builder

--- src/utils/html_builder.cr
+++ src/utils/html_builder.cr
@@ -18,31 +18,65 @@
 # -----------------------------------------------------------------------
 
 module MoonScript
-    class HtmlBuilder
-        def self.build(*, optimize: Bool, doctype: Bool = true, &)
-            html = XML.build_fragment(indent: optimize ? nil : " ") do |xml|
-                builder = new(xml)
-                with builder yield builder
-            end
+  class HtmlBuilder
+    def self.build(*, optimize : Bool, doctype : Bool = true, &)
+      html =
+        XML.build_fragment(indent: optimize ? nil : "  ") do |xml|
+          builder = new(xml)
+
+          with builder yield builder
         end
 
-        def initialize(@xml: XML::Builder)
+      header =
+        if doctype
+          "<!DOCTYPE html>#{optimize ? "" : "\n"}"
         end
 
-        def tag(tag, attributes, &)
-            @xml.element(tag, attributes) { with self yield }
-        end
-
-        def script(**attributes, &)
-            @xml.element("script", **attributes) { with self yield }
-        end
-
-        def script(**attributes)
-            script(**attributes) { text("") }
-        end
-
-        def raw(contents)
-            @xml.raw(contents)
-        end
+      "#{header}#{html}"
     end
+
+    def initialize(@xml : XML::Builder)
+    end
+
+    def tag(tag, attributes, &)
+      @xml.element(tag, attributes) { with self yield }
+    end
+
+    def script(**attributes, &)
+      @xml.element("script", **attributes) { with self yield }
+    end
+
+    def script(**attributes)
+      script(**attributes) { text("") }
+    end
+
+    def a(**attributes, &)
+      @xml.element("a", **attributes) { with self yield }
+    end
+
+    def a(**attributes)
+      a(**attributes) { text("") }
+    end
+
+    def text(contents)
+      @xml.text(contents)
+    end
+
+    def raw(contents)
+      @xml.raw(contents)
+    end
+
+    {% for tag in %w(html head body meta link pre code noscript div span h1 h2
+                    h3 title aside article nav strong) %}
+        def {{tag.id}}(**attributes)
+          @xml.element("{{tag.id}}", **attributes)
+        end
+  
+        def {{tag.id}}(**attributes)
+          @xml.element("{{tag.id}}", **attributes) do
+            with self yield
+          end
+        end
+      {% end %}
+  end
 end
