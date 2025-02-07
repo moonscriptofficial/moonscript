9e219f1b73657035cff915c10e06cfdb475784fa
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-07 21:57:40 +0530

    moon_json: applications[css_prefix]

--- src/moon_json/applications/css_prefix.cr
+++ src/moon_json/applications/css_prefix.cr
@@ -18,3 +18,23 @@
 # -----------------------------------------------------------------------
 
 module MoonScript
+  class MoonJson
+    class Parser
+      def parse_application_css_prefix : String
+        @parser.read_string
+      rescue JSON::ParseException
+        error! :application_css_prefix_invalid do
+          block do
+            text "The"
+            bold "css-prefix field"
+            text "of the"
+            bold "application object"
+            text "should be a string, but it's not:"
+          end
+
+          snippet snippet_data
+        end
+      end
+    end
+  end
+end
