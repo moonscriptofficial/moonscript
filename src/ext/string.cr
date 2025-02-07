60d7a03939866309a995a5885a3d05edf28a055c
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-07 18:47:49 +0530

    ext: string

---
+++ src/ext/string.cr
@@ -0,0 +1,33 @@
+# -----------------------------------------------------------------------
+# This file is part of MoonScript
+#
+# MoonSript is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# MoonSript is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MoonSript.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2025 Krisna Pranav, MoonScript Developers
+# -----------------------------------------------------------------------
+
+class String
+    def uncolorize: String
+    end
+
+    def last? : Char?
+        self[-1]?
+    end
+
+    def indent(spaces: Int32 = 2) : String
+        lines.join('\n') do |line|
+            line.empty? ? line : ("" * spaces) + line
+        end 
+    end
+end
