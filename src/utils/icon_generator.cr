b025ef1519f89b8706bcdd5baa902d688328eea1
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-07 19:02:53 +0530

    utils: icon_generator

--- src/utils/icon_generator.cr
+++ src/utils/icon_generator.cr
@@ -22,10 +22,18 @@ module MoonScript
     extend self
 
     def convert(image, size)
-      output = IO::Memory.new
-      error = IO::Memory.new
+      output =
+        IO::Memory.new
 
-      if status_succes?
+      error =
+        IO::Memory.new
+
+      status =
+        Process.run(
+          "convert #{image} -resize #{size}x#{size} -",
+          shell: true, error: error, output: output)
+
+      if status.success?
         output.to_s
       else
         ""
