14b291da984281c0d1ec0183bf0a9d594cf76010
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-08 20:18:28 +0530

    type_checkers: use

---
+++ src/type_checkers/use.cr
@@ -0,0 +1,67 @@
+# -----------------------------------------------------------------------
+# This file is part of MoonScript
+#
+# MoonSript is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# MoonSript is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MoonSript.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2025 Krisna Pranav, MoonScript Developers
+# -----------------------------------------------------------------------
+
+module MoonScript
+  class TypeChecker
+    def check(node : Ast::Use) : Checkable
+      condition =
+        node.condition
+
+      provider =
+        ast.providers.find(&.name.value.==(node.provider.value))
+
+      error! :use_not_found_provider do
+        snippet "could not find a provider:", node.provider
+      end unless provider
+
+      resolve provider
+
+      lookups[node] = {provider, nil}
+
+      subscription =
+        records.find!(&.name.==(provider.subscription.value))
+
+      record =
+        resolve node.data
+
+      error! :use_subscription_mismatch do
+        block "The subsctipion of provider not matches: "
+        expected subscription, record
+        snippet "The provider in question is here:", node
+      end unless Comparer.compare(record, subscription)
+
+      if condition
+        condition_type = resolve condition
+
+        error! :use_condition_mismatch do
+          block do
+            text "The expression of the"
+            bold "where condition"
+            text "must evaluate to a boolean value: "
+          end
+
+          snippet condition_type
+          snippet "The condition in question is here:", condition
+        end unless Comparer.compare(condition_type, BOOL)
+      end
+
+      resolve node.data
+    end
+  end
+end
