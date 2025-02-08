f0725ad3f676e1ae6ce6baca8cdb8127abda4295
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-08 19:38:42 +0530

    moon_json: test_directories

--- src/moon_json/test_directories.cr
+++ src/moon_json/test_directories.cr
@@ -18,23 +18,86 @@
 # -----------------------------------------------------------------------
 
 module MoonScript
-    class MoonJson
-        class Parser
-            def parse_test_directories: Array(String)
-                @parser.location.try do |location|
-                    directories = %w[]
+  class MoonJson
+    class Parser
+      def parse_test_directories : Array(String)
+        @parser.location.try do |location|
+          directories = %w[]
 
-                    @parser.read_array do
-                      directories << parse_test_directory
-                    end
+          @parser.read_array do
+            directories << parse_test_directory
+          end
 
-                    error! :test_directories_empty do
-                    end
-
-                    block do
-                    end
-                end
+          error! :test_directories_empty do
+            block do
+              text "The"
+              bold "test-directories"
+              text "field lists all directories relative to moon.json"
+              text "which contain the test files."
             end
+
+            block do
+              text "The"
+              bold "test-directories"
+              text "array should not be empty"
+            end
+
+            snippet snippet_data(location)
+          end if directories.empty?
+
+          directories
         end
+      rescue JSON::ParseException
+        error! :test_directories_invalid do
+          block do
+            text "The"
+            bold "test-directories"
+            text "field lists all directories relative to the moon.json"
+            text "which contain the test files."
+          end
+
+          block do
+            text "The"
+            bold "test-directories"
+            text "field should be an array"
+          end
+
+          snippet snippet_data
+        end
+      end
+
+      def parse_test_directory : String
+        location =
+          @parser.location
+
+        directory =
+          @parser.read_string
+
+        path =
+          Path[root, directory]
+
+        error! :test_directory_not_exists do
+          block do
+            text "The test directory"
+            bold directory
+            text "does not exists:"
+          end
+
+          snippet snippet_data(location)
+        end unless Dir.exists?(path)
+
+        directory
+      rescue JSON::ParseException
+        error! :test_directory_invalid do
+          block do
+            text "All entries in the"
+            bold "test-directories"
+            text "array should be string:"
+          end
+
+          snippet snippet_data
+        end
+      end
     end
+  end
 end
