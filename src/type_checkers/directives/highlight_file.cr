5ed6ebd339be1d75944b5b7346689d7b4ff11a39
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-08 19:51:48 +0530

    type_checkers: directives[highlight file]

---
+++ src/type_checkers/directives/highlight_file.cr
@@ -0,0 +1,31 @@
+# -----------------------------------------------------------------------
+# This file is part of MoonScript
+#
+# MoonSript is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# MoonSript is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MoonSript.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2025 Krisna Pranav, MoonScript Developers
+# -----------------------------------------------------------------------
+
+module MoonScript
+  class TypeChecker
+    def check(node : Ast::Directives::HighlightFile) : Checkable
+      error! :highlight_file_directive_expected_file do
+        snippet "The path specified for an highlight file directive does not exist:", node.relative_path
+        snippet "The highlight file: ", node
+      end unless node.exists?
+
+      HTML
+    end
+  end
+end
