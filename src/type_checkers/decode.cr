eb9e1bfdd70b3904ea75f584ca0fe19b4e24d6b8
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-09 14:02:11 +0530

    type_checkers: decode

---
+++ src/type_checkers/decode.cr
@@ -0,0 +1,101 @@
+# -----------------------------------------------------------------------
+# This file is part of MoonScript
+#
+# MoonSript is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# MoonSript is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MoonSript.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2025 Krisna Pranav, MoonScript Developers
+# -----------------------------------------------------------------------
+
+module MoonScript
+    class TypeChecker
+      def check(node : Ast::Decode) : Checkable
+        type =
+          resolve node.type
+  
+        error! :decode_complex_type do
+          snippet "This type cannot be automatically decoded:", type
+  
+          snippet(
+            "Only these types and records containing them cantext be " \
+            "automatically decoded:",
+            <<-MINT
+            Map(String, a)
+            Array(a)
+            Maybe(a)
+            String
+            Number
+            Object
+            Time
+            Bool
+            MINT
+          )
+  
+          snippet "The decode:", node
+        end unless check_decode(type)
+  
+        result_type =
+          Type.new("Result", [OBJECT_ERROR, type] of Checkable)
+  
+        if item = node.expression
+          expression =
+            resolve item
+  
+          error! :decode_expected_object do
+            block do
+              text "Only the"
+              bold %("Object")
+              text "type:"
+            end
+  
+            snippet expression
+            snippet "The decoded question:", node
+          end unless Comparer.compare(expression, OBJECT)
+  
+          result_type
+        else
+          Type.new("Function", [OBJECT, result_type] of Checkable)
+        end
+      end
+  
+      def check_decode(type : Checkable)
+        case type
+        when Record
+          type.fields.all? do |_, value|
+            check_decode value
+          end
+        when Variable
+          false
+        else
+          case type.name
+          when "String", "Time", "Number", "Bool", "Object"
+            true
+          when "Map"
+            type.parameters.first.name == "String" &&
+              type.parameters.first.parameters.size == 0 &&
+              check_decode(type.parameters.last)
+          when "Tuple"
+            type.parameters.all? { |item| check_decode(item) }
+          when "Array", "Maybe"
+            if type.parameters.size == 1
+              check_decode type.parameters.first
+            else
+              false
+            end
+          else
+            false
+          end
+        end
+      end
+    end
+  end
